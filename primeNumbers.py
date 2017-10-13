d6cce1aa7a1831d9a460fd8286d52ae2fd3deee4
Tracy Kaonje Omamo <tomamo@SamaDc.corp>
2017-10-13 17:51:52 +0300

    new variables

--- primeNumbers.py
+++ primeNumbers.py
@@ -4,8 +4,8 @@ def prime_numbers(number):
  for i in range(2, number): # loops through 2 3 4 5 6 7
    prime = True
 
-   for x in range(2, i): # 2,3
-     if i % x == 0:
+   for t in range(2, i): # 2,3
+     if i % t == 0:
        prime = False
 
    if prime == True:
